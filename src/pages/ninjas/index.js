841e50de255c73c977e0a9205e73fb239dcb131e
Ben Graham <bengraham@Bens-MacBook-Air.local>
2023-07-04 11:31:18 +0200

    Lesson 10

--- src/pages/ninjas/index.js
+++ src/pages/ninjas/index.js
@@ -1,12 +1,39 @@
-import Link from 'next/link'
+/*
+ * Fetching Data in Next
+ * The webapp is rendered on the server, so use NextJS function to fetch data
+ * The function runs at build time. Code in the function does not run in the browser
+ * THe function runs before the component is rendered
+ */
+export const getStaticProps = async () => {
+
+	const response = await fetch("https://jsonplaceholder.typicode.com/users")
+
+	const data = await response.json() //^ Returns an array of objects
+
+	return {
+		//^ These are the properties you make accessible inside the component as a prop.
+		props: { ninjas: data }
+	}
+}
+
+
+
 import React from 'react'
+import styles from '../../styles/ninjas.module.css'
 
 
-export default function Index() {
+
+export default function Ninjas( {ninjas}) {
   return (
     <div>
         <h1>All Ninjas</h1>
-        <Link href="/ninjas">See All Ninjas</Link>
+		{ninjas.map(ninja => (
+			<div key={ninja.id}>
+				<a className={styles.single}> 
+					<h3>{ninja.name}</h3>
+				</a>
+			</div>
+		))}
     </div>
   )
 }
